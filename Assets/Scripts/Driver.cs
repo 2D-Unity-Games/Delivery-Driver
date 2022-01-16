96ac051511236622dcbf7709cc455f864fe3e5b7
Stanislav Goncharov <stas76644@icloud.com>
2022-01-16 19:39:02 +0300

    27 Boosts & Dumps

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -6,6 +6,8 @@ public class Driver : MonoBehaviour
 {
 	[SerializeField] float steerSpeed;
 	[SerializeField] float moveSpeed;
+	[SerializeField] float slowSpeed = 15f;
+	[SerializeField] float boostSpeed = 30f;
 
 	private void Update()
 	{
@@ -13,5 +15,16 @@ public class Driver : MonoBehaviour
 		float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
 		transform.Rotate(0, 0, -steerAmount);
 		transform.Translate(0, moveAmount, 0);
+	}
+	
+	private void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Boost")
+		{
+			moveSpeed = boostSpeed;
+		}
+	}
+	
+	private void OnCollisionEnter2D(Collision2D other) {
+		moveSpeed = slowSpeed;
 	}
 }
