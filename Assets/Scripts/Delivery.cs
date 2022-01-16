c9759a0869158008ba44c2f5ece7e1074ef50690
Stanislav Goncharov <stas76644@icloud.com>
2022-01-16 19:17:45 +0300

    24 How to use bool

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+	bool hasPackage;
 	
 	private void OnCollisionEnter2D(Collision2D other) {
 		Debug.Log("Im collided");
@@ -12,7 +13,14 @@ public class Delivery : MonoBehaviour
 	private void OnTriggerEnter2D(Collider2D other) {
 		if (other.tag == "Package")
 		{
+			Debug.Log("Package picked up");
+			hasPackage = true;
+		}
+		
+		if (hasPackage && other.tag == "Package")
+		{
 			Debug.Log("Im triggered");
+			hasPackage = false;
 		}
 	}
 }
